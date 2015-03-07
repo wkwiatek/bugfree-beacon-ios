ba2281af12bfef271a858dc985e06b974ac72881
Wojciech Kwiatek <wojtek.kwiatek@gmail.com>
2015-03-07 17:14:14 +0100

    Ranging and monitoring beacons improved.

--- iBeacons/ViewController.swift
+++ iBeacons/ViewController.swift
@@ -13,7 +13,7 @@ import SwiftyJSON
 
 class ViewController: UIViewController, ESTBeaconManagerDelegate {
     
-    let uuid = "B9407F30-F5F8-466E-AFF9-25556B57FE6D"
+    let uuid = "B9407F30-F5F8-466E-AFF9-25556B57FE6D";
     let beaconManager: ESTBeaconManager = ESTBeaconManager();
     let server: BejkonREST = BejkonREST(host: "http://bejkon.herokuapp.com");
     
@@ -22,17 +22,17 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate {
     ];
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         // Do any additional setup after loading the view, typically from a nib.
         
         beaconManager.delegate = self;
+        beaconManager.requestAlwaysAuthorization();
         
         let region : ESTBeaconRegion = ESTBeaconRegion(proximityUUID: NSUUID(UUIDString: uuid), identifier: "Estimote");
         
         beaconManager.startMonitoringForRegion(region);
-        beaconManager.requestStateForRegion(region);
         beaconManager.startRangingBeaconsInRegion(region);
-        
+
     }
     
     @IBAction func buttonPressed() {
@@ -45,43 +45,23 @@ class ViewController: UIViewController, ESTBeaconManagerDelegate {
     
     func beaconManager(manager: ESTBeaconManager!, didEnterRegion region: ESTBeaconRegion!) {
         println("Beacon entered region");
-    }
-    
-    func beaconManager(manager: ESTBeaconManager!, didExitRegion region: ESTBeaconRegion!) {
-        println("Beacon left region");
         var notification : UILocalNotification = UILocalNotification();
-        notification.alertBody = "The shoes you'd tried on are now 20% off for you with this coupon";
+        notification.alertBody = "Beacon has been found nearby. Check what information it contains.";
         notification.soundName = UILocalNotificationDefaultSoundName;
         UIApplication.sharedApplication().presentLocalNotificationNow(notification);
     }
     
+    func beaconManager(manager: ESTBeaconManager!, didExitRegion region: ESTBeaconRegion!) {
+        println("Beacon left region");
+    }
+    
+    
     func beaconManager(manager: ESTBeaconManager!, didRangeBeacons beacons: [AnyObject], inRegion region: ESTBeaconRegion!) {
+        println("Ranged beacons: \(beacons)");
         let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
         if (knownBeacons.count > 0) {
             let closestBeacon = knownBeacons[0] as ESTBeacon;
             self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue];
-
-            // GET call test
-            var postEndpoint: String = "http://jsonplaceholder.typicode.com/posts/1";
-            Alamofire.request(.GET, postEndpoint)
-                .responseJSON { (request, response, data, error) in
-                    if let anError = error {
-                        println("error calling GET on /posts/1");
-                        println(error);
-                    }
-                    else if let data: AnyObject = data {
-                        let post = JSON(data);
-                        println("The post is: " + post.description)
-                        if let title = post["title"].string {
-                            println("The title is: " + title)
-                        }
-                        else {
-                            println("error parsing /posts/1")
-                        }
-                    }
-            }
-
-
         }
     }
 
