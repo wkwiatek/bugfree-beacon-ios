0f5f1d08910a17bdd7b25b6f014531907ed91800
Norbert <khozzy@gmail.com>
2015-05-01 16:01:30 +0200

    My beacons detailed view. Landscape mode disabled

--- iAdmin/BeaconFeeder.swift
+++ iAdmin/BeaconFeeder.swift
@@ -6,9 +6,7 @@ class BeaconFeeder {
     // Returns some beacons from ranging
     static func feedFromRanging(handler: (beacons: [RangedBeacon]) -> ()) {
         var rangedBeacons = [RangedBeacon]()
-        
-        // TODO: Implement me smarter
-        
+                
         let beacon1 = RangedBeacon(minor: 1, major: 2, uuid: "uuid1")
         let beacon2 = RangedBeacon(minor: 34, major: 522, uuid: "uuid2")
         
@@ -22,11 +20,14 @@ class BeaconFeeder {
     static func feedMyBeacons(handler: (beacons: [MyBeacon]) -> ()) {
         var myBeacons = [MyBeacon]()
         
-        // TODO: Implement me
         let beacon1 = MyBeacon()
         beacon1.title = "Fryzjer"
         beacon1.subtitle = "Odwiedz fryzjera"
         beacon1.content = "Przykladowy content"
+        beacon1.titleColor = "#000000"
+        beacon1.subtitleColor = "#111111"
+        beacon1.contentColor = "#222222"
+        beacon1.backgroundColor = "#FFFFFF"
         
         myBeacons.append(beacon1)
         
--- iAdmin/MyBeaconDetailsViewController.swift
+++ iAdmin/MyBeaconDetailsViewController.swift
@@ -7,6 +7,10 @@ class MyBeaconDetailsViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var subtitleTextField: UITextField!
     @IBOutlet weak var contentTextField: UITextField!
+    @IBOutlet weak var titleColorTextField: UITextField!
+    @IBOutlet weak var subtitleColorTextField: UITextField!
+    @IBOutlet weak var contenColorTextField: UITextField!
+    @IBOutlet weak var backgroundColorTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +21,13 @@ class MyBeaconDetailsViewController: UIViewController {
         titleTextField.text = beacon?.title!
         subtitleTextField.text = beacon?.subtitle!
         contentTextField.text = beacon?.content!
+        titleColorTextField.text = beacon?.titleColor!
+        subtitleColorTextField.text = beacon?.subtitleColor!
+        contenColorTextField.text = beacon?.contentColor!
+        backgroundColorTextField.text = beacon?.backgroundColor!
     }
 
+    @IBAction func saveBeaconData(sender: AnyObject) {
+        println("Saving new beacon data")
+    }
 }
